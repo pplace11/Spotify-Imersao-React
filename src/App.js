4d205bc6e4a179818df4c40baa3570d6086b45c8
Pedro Place <pedro.place@hotmail.com>
2025-01-31 17:09:46 +0000

    Aula 5 + desafio Alura

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Sidebar from './Sidebar/Sidebar';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <div>
+    <Sidebar></Sidebar>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,55 @@
+import './Sidebar.css';
+import logoSpotify from '../assets/icons/logo-spotify.png';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <nav className="sidebar__navigation">
+                <div className="logo">
+                    <a href="">
+                        <img src={logoSpotify} alt="Logo" />
+                    </a>
+                </div>
+                <ul>
+                    <li>
+                        <a href="">
+                            <span>Início</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span>Buscar</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div className="library">
+                <div className="library__content">
+                    <button className="library__button">
+                        <span className="fa fas fa-book"></span>
+                        <span>Sua biblioteca</span>
+                    </button>
+                </div>
+                <section className="section-playlist">
+                    <div className="section-playlist__content">
+                        <span className="text title">Crie sua primeira playlist</span>
+                        <span className="text subtitle">É fácil, vamos te ajudar.</span>
+                        <button className="section-playlist__button">
+                            <span>Criar playlist</span>
+                        </button>
+                    </div>
+                </section>
+                <div className="cookies">
+                    <a href="">Cookies</a>
+                </div>
+                <div className="languages">
+                    <button className="languages__button">
+                        <span>Português do Brasil</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Sidebar;
